1fa7d33a8d7a96cd80730438b2fe98d36ba382d6
Mircea Pricop <mircea.a.pricop@gmail.com>
2012-07-20 15:01:53 +0200

    Update all files with the mutable tree version.

--- src/web/kdTree.js
+++ src/web/kdTree.js
@@ -1,26 +1,135 @@
 function kdTree(points, metric, dimensions) {
 
-  function Node(obj, d) {
+  function Node(obj, d, parent) {
     this.obj = obj;
     this.left = null;
     this.right = null;
+    this.parent = parent;
     this.dimension = d;
   }
 
-  var root = buildTree(points, 0);
+  var self = this;
+  this.root = buildTree(points, 0, null);
 
-  function buildTree(points, depth) {
+  function buildTree(points, depth, parent) {
     var dim = depth % dimensions.length;
     if(points.length == 0) return null;
-    if(points.length == 1) return new Node(points[0], dim);
+    if(points.length == 1) return new Node(points[0], dim, parent);
 
     points.sort(function(a,b){ return a[dimensions[dim]] - b[dimensions[dim]]; });
 
     var median = Math.floor(points.length/2);
-    var node = new Node(points[median], dim);
-    node.left = buildTree(points.slice(0,median), depth+1);
-    node.right = buildTree(points.slice(median+1), depth+1);
+    var node = new Node(points[median], dim, parent);
+    node.left = buildTree(points.slice(0,median), depth+1, node);
+    node.right = buildTree(points.slice(median+1), depth+1, node);
     return node;
+  }
+
+  this.insert = function(point) {
+    var insertPosition = innerSearch(self.root, null);
+
+    if(insertPosition == null) {
+      self.root = new Node(point, 0, null);
+      return;
+    }
+
+    var newNode = new Node(point, (insertPosition.dimension+1)%dimensions.length, insertPosition);
+    var dimension = dimensions[insertPosition.dimension];
+    if(point[dimension] < insertPosition.obj[dimension]) {
+      insertPosition.left = newNode;
+    } else {
+      insertPosition.right = newNode;
+    }
+
+    function innerSearch(node, parent) {
+      if(node == null) return parent;
+
+      var dimension = dimensions[node.dimension];
+      if(point[dimension] < node.obj[dimension]) {
+        return innerSearch(node.left, node);
+      } else {
+        return innerSearch(node.right, node);
+      }
+    }
+  }
+
+  this.remove = function(point) {
+    var node = nodeSearch(self.root);
+    if(node == null) return;
+
+    removeNode(node);
+    function nodeSearch(node) {
+      if(node == null) return null;
+      if(node.obj === point) return node;
+
+      var dimension = dimensions[node.dimension];
+      if(point[dimension] < node.obj[dimension]) {
+        return nodeSearch(node.left, node);
+      } else {
+        return nodeSearch(node.right, node);
+      }
+    }
+
+    function removeNode(node) {
+      if(node.left == null && node.right == null) {
+        if(node.parent == null) {
+          self.root = null;
+          return;
+        }
+        var pDimension = dimensions[node.parent.dimension];
+        if(node.obj[pDimension] < node.parent.obj[pDimension]) {
+          node.parent.left = null;
+        } else {
+          node.parent.right = null;
+        }
+        return;
+      }
+
+      if(node.left != null) {
+        var nextNode = findMax(node.left, node.dimension);
+      } else {
+        var nextNode = findMin(node.right, node.dimension);
+      }
+      var nextObj = nextNode.obj;
+      removeNode(nextNode);
+      node.obj = nextObj;
+
+      function findMax(node, dim) {
+        if(node == null) return null;
+
+        var dimension = dimensions[dim];
+        if(node.dimension == dim) {
+          if(node.right != null) return findMax(node.right, dim);
+          return node;
+        }
+
+        var own = node.obj[dimension]
+        var left = findMax(node.left, dim);
+        var right = findMax(node.right, dim);
+        var max = node;
+        if(left != null && left.obj[dimension] > own) max = left;
+        if(right != null && right.obj[dimension] > max.obj[dimension]) max = right;
+        return max;
+      }
+
+      function findMin(node, dim) {
+        if(node == null) return null;
+
+        var dimension = dimensions[dim];
+        if(node.dimension == dim) {
+          if(node.left != null) return findMin(node.left, dim);
+          return node;
+        }
+
+        var own = node.obj[dimension]
+        var left = findMin(node.left, dim);
+        var right = findMin(node.right, dim);
+        var min = node;
+        if(left != null && left.obj[dimension] < own) min = left;
+        if(right != null && right.obj[dimension] < min.obj[dimension]) min = right;
+        return min;
+      }
+    }
   }
 
   this.nearest = function(point, maxNodes, maxDistance) {
@@ -30,7 +139,7 @@ function kdTree(points, metric, dimensions) {
         bestNodes.push([null, maxDistance]);
       }
     }
-    nearestSearch(root);
+    nearestSearch(self.root);
 
     function nearestSearch(node) {
       var bestChild;
@@ -97,6 +206,20 @@ function kdTree(points, metric, dimensions) {
       }
     }
     return result;
+  }
+
+  this.balanceFactor = function() {
+    return height(self.root)/(Math.log(count(self.root))/Math.log(2));
+
+    function height(node) {
+      if(node == null) return 0;
+      return Math.max(height(node.left), height(node.right)) + 1;
+    }
+
+    function count(node) {
+      if(node == null) return 0;
+      return count(node.left) + count(node.right) + 1;
+    }
   }
 
   // Binary heap implementation from:
